092bccf7fd92393b3844c8614c221e94e78a2628
ellun <eltoncheng86@gmail.com>
2017-06-23 09:50:18 -0400

    updating chef

--- recipes/ckan.rb
+++ recipes/ckan.rb
@@ -3,3 +3,15 @@
 # Recipe:: ckan
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+if Dir.exists? "/home/vagrant"
+    user = "vagrant"
+else
+  user = "ubuntu"
+end
+
+directory "/home/#{user}/uploads" do
+    recursive true
+    mode 0777
+end
